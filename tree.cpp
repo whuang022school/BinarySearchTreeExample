f82fe5c593f0c504e9678970a2d46383734ebe72
= <=>
2018-06-09 16:02:31 +0800

    Add set_data / get_data

--- tree.cpp
+++ tree.cpp
@@ -1,4 +1,20 @@
 #include<iostream>
 #include<cstdio>
-#include<tree.h>
+#include"tree.h"
+
+Tree::Tree(){
+    this->data=0;
+    this->left=NULL;
+    this->right=NULL;
+}
+void Tree::set_data(int data){
+    this->data=data;
+}
+
+int Tree::get_data(){
+    return this->data;
+}
+void Tree::set_left(Tree  ptr){
+    this->left=&ptr;
+}
 
--- tree.h
+++ tree.h
@@ -1,19 +1,14 @@
-#ifdef TREE_H
-#define TREE_H
-
 class Tree{
     private:
     int data;
     Tree * left;
     Tree * right;
     public:
+    Tree();
     int get_data();
-    * Tree get_left();
-    * Tree get_right();
-    void set_left(const Tree * ptr);
-    void set_right(const Tree * ptr);
+    Tree * get_left();
+    Tree * get_right();
+    void set_left(Tree  ptr);
+    void set_right(Tree  ptr);
     void set_data(int data);
-}
-
-#endif
-
+};
